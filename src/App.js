6aadf92053375418767903903dd8a2de6e23c13c
Alexandra Wenner <alexandrawenner05@gmail.com>
2022-06-21 07:53:26 -0400

    got navbar back

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ function App() {
  return (
    <div className="App">
       <div className="Screen">
-          {/* <NavBar /> */}
+          <NavBar /> 
           <Form onAddTask={onAddTask}/>
 
           {/* <Switch>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,25 +1,38 @@
 import React from 'react';
-// import {NavLink} from 'react-router-dom'
+import {NavLink} from 'react-router-dom'
 
 function NavBar () {
     return(
-        <div id="routes">
-        {/* //     <NavLink exact className="button" to="/">
-        //       App
-        //     </NavLink>
-        //     <NavLink exact className="button" to="/Monday">
-        //       Monday
-        //     </NavLink>
-        //     <NavLink exact className="button" to="/Tuesday">
-        //       Tuesday
-        //     </NavLink>
-        //     <NavLink exact className="button" to="/Wednesday">
-        //       Wednesday
-        //     </NavLink>
-        //     <NavLink exact className="button" to="/Thursday">
-        //       Thursday
-        //     </NavLink> */}
-        // </div>
+        <header>
+            <nav>
+                <div id="routes">
+                   <NavLink exact className="button-even" to="/">
+                        App
+                    </NavLink>
+                    <NavLink exact className="button-odd" to="/Monday">
+                        Monday
+                    </NavLink>
+                    <NavLink exact className="button-even" to="/Tuesday">
+                        Tuesday
+                    </NavLink>
+                    <NavLink exact className="button-odd" to="/Wednesday">
+                        Wednesday
+                    </NavLink>
+                    <NavLink exact className="button-even" to="/Thursday">
+                        Thursday
+                    </NavLink>   
+                    <NavLink exact className="button-odd" to="/Friday">
+                        Friday
+                    </NavLink>   
+                    <NavLink exact className="button-even" to="/Saturday">
+                        Saturday
+                    </NavLink>   
+                    <NavLink exact className="button-odd" to="/Sunday">
+                        Sunday
+                    </NavLink>   
+                </div>
+            </nav>
+        </header>
     )
 }
 
