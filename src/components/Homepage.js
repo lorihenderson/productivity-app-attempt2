809c5f2ea13f931daa195f619358724fa373dc3b
Lori Henderson <lorim95@gmail.com>
2022-06-22 14:51:53 -0400

    styled background and buttons

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -3,7 +3,24 @@ import React from 'react';
 function Homepage() {
 
     return(
-        <div className="homepage">Welcome to our sexy app</div>
+        <div className="homepage"> Welcome to our sexy app
+        <blockquote className='quotes'>
+            It is during our darkest moments that we must focus to see the light.
+            <br/>
+            ~Aristotle~
+        </blockquote>
+        <blockquote className='quotes'>
+            The only true wisdom is in knowing you know nothing.
+            <br/>
+            ~Socrates~
+        </blockquote>
+        <blockquote className='quotes'>
+            Have you tried it?
+            <br/>
+            ~Antonio Reid~
+        </blockquote>
+        <span>We miss you Antonio &#129496;</span>
+        </div>
     )
 }
 export default Homepage;
